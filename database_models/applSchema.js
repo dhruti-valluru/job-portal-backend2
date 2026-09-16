34f850086fbac60f4d92f8b8c767bfa70134a50b
dhruti-valluru <25071a05p3@vnrvjiet.in>
2026-09-16 22:56:37 +0530

    Add database models

---
+++ database_models/applSchema.js
@@ -0,0 +1,38 @@
+import { Schema, model, Types } from "mongoose";
+
+const applicationSchemaDefinition = new Schema(
+  {
+    job: {
+      type: Types.ObjectId,
+      ref: "job",
+      required: true
+    },
+    jobSeeker: {
+      type: Types.ObjectId,
+      ref: "user",
+      required: true
+    },
+    resume: {
+      type: String,
+      trim: true
+    },
+    coverLetter: {
+      type: String,
+      trim: true
+    },
+    status: {
+      type: String,
+      enum: ["submitted", "reviewing", "shortlisted", "rejected", "hired"],
+      default: "submitted"
+    }
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+    strict: "throw"
+  }
+);
+
+applicationSchemaDefinition.index({ job: 1, jobSeeker: 1 }, { unique: true });
+
+export const ApplicationModel = model("application", applicationSchemaDefinition);
